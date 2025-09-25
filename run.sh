bb308a91209948c200dd6cfcbeba1e92bc28cad3
Bartłomiej Sobieszek <contact@protean.pl>
2025-09-25 10:58:43 +0200

    composer.lock

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm --name cipherio -v $(pwd):/app -w /app php:8.4-cli-alpine3.22 php index.php
+docker run --rm --name cipherio -v $(pwd):/app -w /app composer:2.8.12 composer install
+docker run --rm --name cipherio -v $(pwd):/app -w /app composer:2.8.12 php index.php
